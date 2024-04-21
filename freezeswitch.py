ed23c06a6d6251535df7d7e61479c250bf089999
LukeDevsE <108841495+LukeDevsE@users.noreply.github.com>
2024-04-20 21:37:33 -0600

    Update freezeswitch.py v2.0.0
    source code for the update

--- freezeswitch.py
+++ freezeswitch.py
@@ -1,18 +1,18 @@
 import psutil
-from PyQt5.QtWidgets import QApplication,QWidget,QVBoxLayout,QComboBox,QCheckBox
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication,QWidget,QVBoxLayout,QComboBox,QCheckBox,QSpinBox,QLabel,QPushButton
+from PyQt5.QtCore import Qt,QEventLoop,QTimer
 import keyboard
 import qdarkstyle
+from time import sleep
 # handle = win32process.OpenProcess(win32process.PROCESS_ALL_ACCESS, False, 12824)
-
-
-
-
 def get_process_id_by_name(name):
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == name:
             return proc.info['pid']
     return None
+
+pid = get_process_id_by_name("RobloxPlayerBeta.exe")
+
 def main():
     app = QApplication([])
     app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
@@ -22,33 +22,83 @@ def main():
 
     layout = QVBoxLayout()
     text = QComboBox()
+    label = QLabel()
+    label.setText("Seconds Default 7, 0 to disable. this waits a set amount of time and unfreezes")
+    cooldown = QSpinBox()
+    cooldown.setValue(7)
     text.addItems(["`","1","2","3","4","5","6","7","8","9","0","-","=","q","e","r","t","y","u","i","o","p"])
     text.setCurrentText("`")
     button = QCheckBox("Freeze Roblox!")
-    button.clicked.connect(lambda: toggle(button.isChecked()))
-    keyboard.hook(lambda event: Toggletoggle(button,text.currentText()))
+    button.clicked.connect(lambda: toggle(button.isChecked(),cooldown.value(),button))
+    keyboard.hook(lambda event: Toggletoggle(button,text.currentText(),cooldown.value()))
     layout.addWidget(button)
     layout.addWidget(text)
+    layout.addWidget(label)
+    layout.addWidget(cooldown)
     window.setLayout(layout)
     window.show()
     window.setGeometry(638,218,149,104)
     app.exec_()
 
 
-def Toggletoggle(button: QCheckBox, key):
-    if keyboard.is_pressed(key):
+def Toggletoggle(button: QCheckBox, key, cd):
+    #global pid  # Added global declaration
+    if keyboard.is_pressed(key) and button.isEnabled() and cd > 0:
         button.setChecked(not button.isChecked())
-        toggle(button.isChecked())
-def toggle(checked):
+        toggle(button.isChecked(),cd,button)
+    elif keyboard.is_pressed(key) and button.isEnabled() and cd == 0:
+        button.setChecked(not button.isChecked())
+        toggle(button.isChecked(),cd,button)
+
+        
+def toggle(checked, cd, button: QCheckBox):
+    pid = globals()["pid"]
+  # Added global declaration
+    print(pid)
     if (checked):
-        pid = get_process_id_by_name("RobloxPlayerBeta.exe")
         if pid != None:
-            process = psutil.Process(pid)
-            process.suspend()
+            try:
+                process = psutil.Process(pid)
+                process.suspend()
+            except:
+                print("user closed program")
+        else:
+            globals()["pid"] = get_process_id_by_name("RobloxPlayerBeta.exe")
+            pid = globals()["pid"]
+            if pid != None:
+                try:
+                    process = psutil.Process(pid)
+                    process.suspend()
+                except:
+                    print("user closed program")
+        if cd > 0:
+            button.setDisabled(True)
+            loop = QEventLoop()
+            QTimer.singleShot(cd * 1000, loop.quit)
+            loop.exec_()
+            button.setDisabled(False)
+            if pid != None:
+                try:
+                    process = psutil.Process(pid)
+                    process.resume()
+                except:
+                    print("user closed program")
+            button.setChecked(False)
     else:
-        pid = get_process_id_by_name("RobloxPlayerBeta.exe")
         if pid != None:
-            process = psutil.Process(pid)
-            process.resume()
+            try:
+                process = psutil.Process(pid)
+                process.resume()
+                pid = None
+            except:
+                print("user closed program")
+        else:
+            pid = get_process_id_by_name("RobloxPlayerBeta.exe")
+            if pid != None:
+                try:
+                    process = psutil.Process(pid)
+                    process.resume()
+                except:
+                    print("user closed program")
 if __name__ == "__main__":
     main()
